05b174b13b5c7048cc1915985f1159a57c2d377e
Silverlined <dimitriy.georgiev@gmail.com>
2021-03-09 13:38:05 +0100

    Add basic read, write

--- read_pwm.ino
+++ read_pwm.ino
@@ -1,24 +1,12 @@
-#include <Servo.h>
-
-#define READ_PWM 0
-
-// Calibrated pulse width for FrSky Taranis Q X7 16CH 2.4Ghz transmitter with RX8R receiver
-const uint16_t RC_min[4] = {1000, 1000, 1000, 1000};
-const uint16_t RC_mid[4] = {1500, 1500, 1500, 1500};
-const uint16_t RC_max[4] = {2000, 2000, 2000, 2000};
-
 uint8_t last_channel_1_state = 0, last_channel_2_state = 0, last_channel_3_state = 0, last_channel_4_state = 0;
 uint16_t receiver_input_channel_1, receiver_input_channel_2, receiver_input_channel_3, receiver_input_channel_4;
 uint32_t timer_channel_1, timer_channel_2, timer_channel_3, timer_channel_4;
-
-uint32_t last_rc_update = 0;
-Servo motor_left, motor_right, toggle_left, toggle_right;
 
 // Interrupt service routine called evey time digital input pin 8, 9, 10, or 11 changes state
 // PCINT0_vect is the compiler vector for PCINT0 on PORTB of ATmega328
 ISR(PCINT0_vect)
 {
-    // Channel 1 (pin 8) ==================================
+    // Channel 1==================================
     if (last_channel_1_state == 0 && PINB & B00000001)
     {
         last_channel_1_state = 1;
@@ -29,7 +17,7 @@ ISR(PCINT0_vect)
         last_channel_1_state = 0;
         receiver_input_channel_1 = micros() - timer_channel_1;
     }
-    // Channel 2 (pin 9) ==================================
+    // Channel 2==================================
     if (last_channel_2_state == 0 && PINB & B00000010)
     {
         last_channel_2_state = 1;
@@ -38,9 +26,9 @@ ISR(PCINT0_vect)
     if (last_channel_2_state == 1 && !(PINB & B00000010))
     {
         last_channel_2_state = 0;
-        receiver_input_channel_2 = micros() - timer_channel_2;
+        receiver_input_channel_2 = micros() - timer_channel_2
     }
-    // Channel 3 (pin 10) ==================================
+    // Channel 3==================================
     if (last_channel_3_state == 0 && PINB & B00000100)
     {
         last_channel_3_state = 1;
@@ -51,7 +39,7 @@ ISR(PCINT0_vect)
         last_channel_3_state = 0;
         receiver_input_channel_3 = micros() - timer_channel_3;
     }
-    // Channel 4 (pin 11) ==================================
+    // Channel 4==================================
     if (last_channel_4_state == 0 && PINB & B00001000)
     {
         last_channel_4_state = 1;
@@ -112,28 +100,11 @@ void setup()
     PCMSK0 |= (1 << PCINT1); // set PCINT0 (digital pin 9) to trigger an interrupt on state change
     PCMSK0 |= (1 << PCINT2); // set PCINT0 (digital pin 10) to trigger an interrupt on state change
     PCMSK0 |= (1 << PCINT3); // set PCINT0 (digital pin 11) to trigger an interrupt on state change
-
-    motor_left.attach(9, RC_min[1], RC_max[1]);
-    motor_right.attach(10, RC_min[2], RC_max[2]);
-
-    toggle_left.attach(8, RC_min[0], RC_max[0]);
-    toggle_right.attach(11, RC_min[3], RC_max[3]);
-
     Serial.begin(115200);
 }
 
 void loop()
 {
-    if (millis() - last_rc_update > 25)
-    {
-        motor_left.writeMicroseconds(receiver_input_channel_2);
-        motor_right.writeMicroseconds(receiver_input_channel_3);
-        last_rc_update = millis();
-
-        if (READ_PWM)
-        {
-            read_receiver_signals();
-            delay(250);
-        }
-    }
+    delay(250);
+    read_receiver_signals();
 }
